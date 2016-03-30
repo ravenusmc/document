ce48caa8e6fdd5227d70052baa8740424715fa8b
ravenusmc <mcuddy77@gmail.com>
2016-03-30 15:23:24 -0400

    change root of application for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   root 'welcome#index'
 
   resources :docs
+
+  authenticated :user do 
+    root "docs#index", as: "authenticated_root"
+  end 
+
+  devise_for :users
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
